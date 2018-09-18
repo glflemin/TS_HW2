238bb467a1c7d7a02e1ba059ce41d4154bb05bde
wjjenista <42449580+wjjenista@users.noreply.github.com>
2018-09-18 11:30:58 -0400

    stationary testing code

--- TS-HW3.R
+++ TS-HW3.R
@@ -59,20 +59,22 @@ diff(well_ts, lag = 12)
 
 #ADF TESTING#
 #Check rho with lag0
-adf.test(well_ts, alternative = "stationary", k = 0)
+#adf.test(well_ts, alternative = "stationary", k = 0)
 
-# check Tau with other lags (1-5), though Simmons said industry never checks more than "lag2" 
-ADF.Pvalues <- rep(NA, 6)
-for(i in 1:5){
+# check Tau with other lags (0-2), though Simmons said industry never checks more than "lag2" 
+ADF.Pvalues <- rep(NA, 3)
+for(i in 0:2){
   ADF.Pvalues[i+1] <- adf.test(well_ts, alternative = "stationary", k = i)$p.value
 }
 ADF.Pvalues
 
-ndiffs(well_ts)     ## Result of 1 indicates 1 difference required for stationarity
+ndiffs(well_ts, test="adf")     ## Result of 0
+ndiffs(well_ts, test="kpss")    ## Result of 1 indicates 1 difference required for stationarity
+ndiffs(well_ts, test="pp")      ## Result of 0
 
 # IF FIT STOCHASTIC TREND 
 # Take differences
-diff(well_ts)
+ndiffs(diff(well_ts))
 z <- ts(diff(well_ts), start=C(2007,10), frequency = 12)  ## Test for plotting
 
 # IF DETERMINISTIC TREND (all rho/tau p-values < alpha)
@@ -86,6 +88,6 @@ lm(well_ts ~ time_variable)
 # PLOT STATIONARY TIME SERIES
 # appears stationary around y = 1.0
 plot(well_ts, xlab='Time',ylab='Change in Height (Ft)',main='Stationary Well Time Series Graph')
-plot(z, xlab='Time',ylab='Change in Height (Ft)',main='First Difference Well Time Series Graph')   ## test   
+plot(diff(well_ts), xlab='Time',ylab='Change in Height (Ft)',main='First Difference Well Time Series Graph')   ## test   
 # CLEAN ENVIRONMENT
 #rm(list=ls(-ts.final))
